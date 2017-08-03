b3771fc230e3c250dc4ef7391421b81dcf8e8a02
Jorge Durán <jdp0990@gmail.com>
2017-08-02 22:31:16 -0500

    Until class 28

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 const API_KEY = 'AIzaSyCqOHk0gZHrKAG2KD_Sqv4EgC4I9xLfGBs';
 
-const App = () => <div>
-    <SearchBar />
-  </div>;
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({ key: API_KEY, term: 'surfboards' }, videos => this.setState({ videos }));
+  }
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoDetail video={this.state.videos[0]} />
+        <VideoList videos={this.state.videos} />
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(<App />, document.querySelector('.container'));
